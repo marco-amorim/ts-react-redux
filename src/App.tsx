2ee9dd119792801c6bc7417eed5dfef2ee948e2c
marco-amorim <marcoamorim95@hotmail.com>
2021-01-21 10:07:46 -0300

    using redux with hooks on typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { addNote } from './actions';
+import { NewNote } from './components/NewNote';
+import { NotesState } from './notesReducer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const notes = useSelector<NotesState, NotesState['notes']>(
+		(state) => state.notes
+	);
+	const dispatch = useDispatch();
+
+	const onAddNote = (note: string) => {
+		dispatch(addNote(note));
+	};
+
+	return (
+		<React.Fragment>
+			<NewNote addNote={onAddNote} />
+			<hr />
+			<ul>
+				{notes.map((note) => {
+					return <li key={note}>{note}</li>;
+				})}
+			</ul>
+		</React.Fragment>
+	);
 }
 
 export default App;
